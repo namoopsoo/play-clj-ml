bcfc435e9d97e538c048f99901b8cc51d71280af
Michal Piekarczyk <namoopsoo>
2017-10-14 16:06:58 -0400

    trying the lambda-ml decision tree based classifiers

--- matrix-project/src/matrix_project/core.clj
+++ matrix-project/src/matrix_project/core.clj
@@ -1,16 +1,35 @@
 (ns matrix-project.core
 
   (:use [incanter.charts :only [xy-plot add-points scatter-plot add-lines]]
-        [incanter.core :only [view]]
+
+        ;[incanter.core :only [view]]
         [incanter.stats :only [linear-model]]
+
+        [lambda-ml.random-forest :only [
+                                        make-random-forest-classifier
+                                        random-forest-fit
+                                        random-forest-predict
+
+                                        ]]
+        [lambda-ml.decision-tree :only [
+                                        make-classification-tree
+                                        decision-tree-fit
+                                        decision-tree-predict
+                                        gini-impurity
+                                        ]]
         )
    (:require
-             [clojure.core.matrix :as mtrix]
-             [clojure.core.matrix.operators :as M]
-             [clatrix.core :as cl]
-             
-             [clojure.data.csv :as csv]
-             [clojure.java.io :as io]) 
+     [clojure.core.matrix :as mtrix]
+     [clojure.core.matrix.operators :as M]
+     [clatrix.core :as cl]
+     
+     [clojure.data.csv :as csv]
+     [clojure.java.io :as io]
+
+     [incanter.core :as incore]
+     [incanter.io :as incio]
+
+     ) 
   )
 
 (defn pass
@@ -22,7 +41,7 @@
   [s]
   (let [X (concat (:hidden s) (:observed s))
         Y (concat (:hidden-values s) (:observed-values s))]
-    (view 
+    (incore/view 
       (add-points
         (xy-plot X Y) (:observed s) (:observed-values s)))))
         
@@ -33,7 +52,14 @@
 (def fname-tiny
   "/Users/michal/LeDropbox/Dropbox/Code/repo/learn-citibike/datas/201510-citibike-tripdata.simple.tiny.csv")
 
+(def fname-medium
+  "/Users/michal/LeDropbox/Dropbox/Code/repo/learn-citibike/datas/201510-citibike-tripdata.medium-simple.csv")
 
+
+(def fname-medium-train
+  "/Users/michal/LeDropbox/Dropbox/Code/repo/learn-citibike/datas/201510-citibike-tripdata.medium-simple-train.csv")
+(def fname-medium-holdout
+  "/Users/michal/LeDropbox/Dropbox/Code/repo/learn-citibike/datas/201510-citibike-tripdata.medium-simple-holdout.csv")
 
 
 (defn parse-row-as-float
@@ -62,7 +88,33 @@
      :columns columns}
     ))
 
+
+(defn csv-data->maps
+  "Make sequence of maps from input csv data.
+  
+  Lifted from examples in https://github.com/clojure/data.csv"
+  [csv-data]
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (map keyword) ;; Drop if you want string keys instead
+            repeat)
+	  (rest csv-data)))
+
+
+(defn load-csv-data-to-maps
+  [fname]
+  (with-open [reader (io/reader fname)]
+                (->>
+                  (csv/read-csv reader)
+                  (mapv pass)
+                  (csv-data->maps)
+                  )))
+
+
 (defn make-super-simple-linear-model
+  "Bake a single input variable linear regression.
+  
+  Let input be a map with :header and :columns"
   [data-table]
   (let [
         Y (nth (:columns data-table) 2)
@@ -85,17 +137,6 @@
       )))
 
 
-(defn do-training-set-eval-model
-  "Use linear model coefficients to solve for Y
-  for all the data in the training set.
-  Then find the differences in true Y with the predicted.
-  "
-  [training-set simple-linear-model]
- 
-   
-  
-  )
-
 (defn plot-data-and-linear-model
   [X Y simple-model n]
   (do
@@ -103,8 +144,63 @@
     (def linear-simple-scatter (xy-plot (take n X) (take n Y)))
 
     ; then add-lines from fitted model.
-    (view (add-lines linear-simple-scatter (take n X) (take n (:fitted simple-model))))
+    (incore/view (add-lines linear-simple-scatter (take n X) (take n (:fitted simple-model))))
     ))
+
+(comment
+  "Ah, this is the incanter way of reading a csv..."
+
+  (def data-file "data/all_160_in_51.P35.csv")
+  (def va-data (incio/read-dataset data-file :header true))
+  )
+
+
+(defn take-subset-dataset
+  [dataset indices]
+  (let [indices-set (set indices)]
+    (incore/$where  
+     {:index {:$fn (fn [x]
+                     (= (conj indices-set x) indices-set)
+                     )}} 
+     dataset)) 
+  )
+
+
+(def data [[0 0 0] [0 1 1] [1 0 1] [1 1 0]])
+(def fit
+  (let [n 1001
+        min-split 2
+        min-leaf 1
+        max-features 2]
+    (-> (make-random-forest-classifier n min-split min-leaf max-features)
+        (random-forest-fit data))))
+
+
+(defn do-forest-train-classifier
+  [training-matrix]
+  (let [n 10  ;this didnt work though...
+        min-split 2
+        min-leaf 1
+        max-features 2]
+    (-> (make-random-forest-classifier n min-split min-leaf max-features)
+        (random-forest-fit training-matrix)))
+  )
+
+
+(defn do-tree-train-classifier
+  [training-matrix]
+  (let [min-split 2
+        min-leaf 1
+        max-features 2]
+    (-> (make-classification-tree gini-impurity min-split min-leaf max-features)
+        (decision-tree-fit training-matrix)))
+  )
+
+
+(defn train-holdout-split
+  [X]
+  
+  )
 
 
 
